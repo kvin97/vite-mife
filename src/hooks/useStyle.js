36f678a4a1c5773b6d3bfe91761d7caff9404af2
Kavindu Vindika <100210305+kvin5529@users.noreply.github.com>
2024-02-20 12:16:44 +0530

    fix: remove unnecessary use-state to track added style tag

--- src/hooks/useStyle.js
+++ src/hooks/useStyle.js
@@ -1,18 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 function useStyle(styles) {
-  const [styleElement, setStyleElement] = useState(null);
-
   useEffect(() => {
     const styleTag = document.createElement("style");
     styleTag.id = "dynamic-update-styles";
     styleTag.innerHTML = styles;
 
-    setStyleElement(styleTag);
-
     document.head.appendChild(styleTag);
+
     return () => {
-      document.head.removeChild(styleElement);
+      document.head.removeChild(styleTag);
     };
   }, []);
 }
